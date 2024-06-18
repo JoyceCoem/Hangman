169c23c5e78f1efc7ede72dce34907867e04cdcc
Joyce Coem <jc12.jc52@gmail.com>
2024-06-18 13:48:47 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace HangmanProject
+{
+    partial class HangmanGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HangmanGame));
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblWelcome = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.HangmanPost = new System.Windows.Forms.Panel();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lstBxhighScore = new System.Windows.Forms.ListBox();
+            this.lstBxscoreboard = new System.Windows.Forms.ListBox();
+            this.lblLives = new System.Windows.Forms.Label();
+            this.lblMissed = new System.Windows.Forms.Label();
+            this.lblNumber = new System.Windows.Forms.Label();
+            this.lblSize = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.txtLetter = new System.Windows.Forms.TextBox();
+            this.btnRestart = new System.Windows.Forms.Button();
+            this.btnGuess = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.btnStart.ForeColor = System.Drawing.Color.MediumTurquoise;
+            this.btnStart.Location = new System.Drawing.Point(206, 185);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(283, 34);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.Snow;
+            this.btnExit.ForeColor = System.Drawing.Color.Red;
+            this.btnExit.Location = new System.Drawing.Point(247, 255);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(220, 34);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblWelcome
+            // 
+            this.lblWelcome.AutoSize = true;
+            this.lblWelcome.BackColor = System.Drawing.Color.Transparent;
+            this.lblWelcome.Font = new System.Drawing.Font("Tempus Sans ITC", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWelcome.ForeColor = System.Drawing.Color.Red;
+            this.lblWelcome.Location = new System.Drawing.Point(105, 52);
+            this.lblWelcome.Name = "lblWelcome";
+            this.lblWelcome.Size = new System.Drawing.Size(520, 62);
+            this.lblWelcome.TabIndex = 2;
+            this.lblWelcome.Text = "Welcome to Hangman ";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Controls.Add(this.HangmanPost);
+            this.groupBox1.Location = new System.Drawing.Point(6, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(221, 252);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Visible = false;
+            // 
+            // HangmanPost
+            // 
+            this.HangmanPost.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.HangmanPost.Location = new System.Drawing.Point(3, 16);
+            this.HangmanPost.Name = "HangmanPost";
+            this.HangmanPost.Size = new System.Drawing.Size(215, 233);
+            this.HangmanPost.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox2.Controls.Add(this.lstBxhighScore);
+            this.groupBox2.Controls.Add(this.lstBxscoreboard);
+            this.groupBox2.Controls.Add(this.lblLives);
+            this.groupBox2.Controls.Add(this.lblMissed);
+            this.groupBox2.Controls.Add(this.lblNumber);
+            this.groupBox2.Controls.Add(this.lblSize);
+            this.groupBox2.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.groupBox2.Location = new System.Drawing.Point(233, 13);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(438, 252);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Visible = false;
+            // 
+            // lstBxhighScore
+            // 
+            this.lstBxhighScore.FormattingEnabled = true;
+            this.lstBxhighScore.Location = new System.Drawing.Point(331, 16);
+            this.lstBxhighScore.Name = "lstBxhighScore";
+            this.lstBxhighScore.Size = new System.Drawing.Size(101, 43);
+            this.lstBxhighScore.TabIndex = 7;
+            // 
+            // lstBxscoreboard
+            // 
+            this.lstBxscoreboard.FormattingEnabled = true;
+            this.lstBxscoreboard.Location = new System.Drawing.Point(202, 16);
+            this.lstBxscoreboard.Name = "lstBxscoreboard";
+            this.lstBxscoreboard.Size = new System.Drawing.Size(123, 69);
+            this.lstBxscoreboard.TabIndex = 6;
+            // 
+            // lblLives
+            // 
+            this.lblLives.AutoSize = true;
+            this.lblLives.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblLives.Location = new System.Drawing.Point(330, 215);
+            this.lblLives.Name = "lblLives";
+            this.lblLives.Size = new System.Drawing.Size(56, 13);
+            this.lblLives.TabIndex = 3;
+            this.lblLives.Text = "Lives Left:";
+            // 
+            // lblMissed
+            // 
+            this.lblMissed.AutoSize = true;
+            this.lblMissed.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblMissed.Location = new System.Drawing.Point(3, 215);
+            this.lblMissed.Name = "lblMissed";
+            this.lblMissed.Size = new System.Drawing.Size(72, 13);
+            this.lblMissed.TabIndex = 2;
+            this.lblMissed.Text = "Missed Word:";
+            // 
+            // lblNumber
+            // 
+            this.lblNumber.AutoSize = true;
+            this.lblNumber.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblNumber.Location = new System.Drawing.Point(75, 20);
+            this.lblNumber.Name = "lblNumber";
+            this.lblNumber.Size = new System.Drawing.Size(0, 13);
+            this.lblNumber.TabIndex = 1;
+            // 
+            // lblSize
+            // 
+            this.lblSize.AutoSize = true;
+            this.lblSize.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblSize.Location = new System.Drawing.Point(7, 20);
+            this.lblSize.Name = "lblSize";
+            this.lblSize.Size = new System.Drawing.Size(72, 13);
+            this.lblSize.TabIndex = 0;
+            this.lblSize.Text = "Word Length:";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox3.Controls.Add(this.txtLetter);
+            this.groupBox3.Controls.Add(this.btnRestart);
+            this.groupBox3.Controls.Add(this.btnGuess);
+            this.groupBox3.Location = new System.Drawing.Point(12, 282);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(659, 100);
+            this.groupBox3.TabIndex = 5;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Visible = false;
+            // 
+            // txtLetter
+            // 
+            this.txtLetter.Location = new System.Drawing.Point(266, 42);
+            this.txtLetter.Name = "txtLetter";
+            this.txtLetter.Size = new System.Drawing.Size(113, 20);
+            this.txtLetter.TabIndex = 2;
+            // 
+            // btnRestart
+            // 
+            this.btnRestart.Location = new System.Drawing.Point(493, 40);
+            this.btnRestart.Name = "btnRestart";
+            this.btnRestart.Size = new System.Drawing.Size(120, 23);
+            this.btnRestart.TabIndex = 1;
+            this.btnRestart.Text = "Restart";
+            this.btnRestart.UseVisualStyleBackColor = true;
+            this.btnRestart.Click += new System.EventHandler(this.btnRestart_Click);
+            // 
+            // btnGuess
+            // 
+            this.btnGuess.Location = new System.Drawing.Point(14, 40);
+            this.btnGuess.Name = "btnGuess";
+            this.btnGuess.Size = new System.Drawing.Size(120, 23);
+            this.btnGuess.TabIndex = 0;
+            this.btnGuess.Text = "Guess";
+            this.btnGuess.UseVisualStyleBackColor = true;
+            this.btnGuess.Click += new System.EventHandler(this.btnGuess_Click);
+            // 
+            // HangmanGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(683, 394);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lblWelcome);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnStart);
+            this.MaximizeBox = false;
+            this.Name = "HangmanGame";
+            this.Text = "Hangman";
+            this.Shown += new System.EventHandler(this.HangmanGame_Shown);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblWelcome;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Panel HangmanPost;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox txtLetter;
+        private System.Windows.Forms.Button btnRestart;
+        private System.Windows.Forms.Button btnGuess;
+        private System.Windows.Forms.Label lblLives;
+        private System.Windows.Forms.Label lblMissed;
+        private System.Windows.Forms.Label lblNumber;
+        private System.Windows.Forms.Label lblSize;
+        private System.Windows.Forms.ListBox lstBxhighScore;
+        private System.Windows.Forms.ListBox lstBxscoreboard;
+    }
+
+}
+
---
+++ Form1.cs
@@ -0,0 +1,392 @@
+﻿/*  Filename: HangmanProject.sln
+ *  Author: Joyce Koloando Coem
+ *  Created: 11/04/2023
+ *  Description: This program is composed of all the methods and constructor  that will be use to build the game  "Hangman".
+*/
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+
+namespace HangmanProject
+{
+    public partial class HangmanGame : Form
+    {
+        public HangmanGame()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            
+            btnStart.Visible = false;//this line will make the start button to invisible
+            btnExit.Visible = false;//this line will make the exit button to invisible
+            lblWelcome.Visible = false;//this line will make the lblWelcome to invisible
+            groupBox2.Visible = true;//this line will make the groupBox2 to visible
+            groupBox1.Visible = true;//this line will make the groupBox1 to visible
+            groupBox3.Visible = true;//this line will make the groupBox3 to visible
+            MessageBox.Show("Welcome to HangMan, Which color do you think it is?", "Hangman", MessageBoxButtons.OKCancel);// this line will show a messagebox to welcome the user
+            DrawHang();// this line will call the drawHang method to draw the post
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();// this line will exit or close the application
+        }
+        // this variable has been initiased to keep track of which bodypart needs to be drawn
+        int Number = 0;
+        //this initialize variable will be use later on .
+        string Colors = "";
+        // initialized for future use.
+        int score = 0;
+
+        //this line create a new list of labels which will be represent by underscores.
+        List<Label> List = new List<Label>();
+
+        //creating Arraylist to keep track of correct and incorrect answers
+        ArrayList correctAnswer = new ArrayList();
+        ArrayList incorrectAnswer = new ArrayList();
+
+
+        //creating sortedList for the scoreBoard and the highScores
+        IDictionary<string, int> scores = new Dictionary<string, int>();
+        IDictionary<string, int> highScores = new Dictionary<string, int>();
+
+
+
+
+
+        //this enumaration is used to store all the bodyparts taht will be drawn.
+        enum bodyOfHangnam
+        {
+            head,
+            leftEye,
+            rightEye,
+            mouth,
+            leftArm,
+            rightArm,
+            body,
+            leftLeg,
+            rightLeg,
+            nose
+        }
+
+
+
+
+        //this method will create or Draw the hang post .
+        void DrawHang()
+        {
+           
+            Graphics g = HangmanPost.CreateGraphics();// this graphcs will be draw inside the panel(hangmanPost)
+            Pen p = new Pen(Color.Yellow, 10);// the pen used to draw with 
+            g.DrawLine(p, new Point(130, 218), new Point(130, 5));
+            g.DrawLine(p, new Point(135, 5), new Point(65, 5));
+            g.DrawLine(p, new Point(60, 0), new Point(60, 50));
+            DesignLabel();// this calls the method that creates labels
+            // message used to welcome the Player
+            
+
+
+
+        }
+
+
+
+
+
+
+        //the code will draw body of the man  and will use the enum a name 
+        void DrawBodyOfHangman(bodyOfHangnam bh)
+        {
+            
+            Graphics g = HangmanPost.CreateGraphics();
+            Pen p = new Pen(Color.Red, 5);
+
+            // using the if/else if to check which part am i drawing
+            if (bh == bodyOfHangnam.head)// draw the head
+                g.DrawEllipse(p, 40, 50, 40, 40);
+
+            else if (bh == bodyOfHangnam.leftEye)//draw the left eye with the ellipse that will draw small square
+            {
+                SolidBrush s = new SolidBrush(Color.Black);
+                g.FillEllipse(s, 50, 60, 5, 5);
+            }
+            else if (bh == bodyOfHangnam.rightEye)//draw the right eye with the ellipse that will draw small square
+            {
+                SolidBrush s = new SolidBrush(Color.Black);
+                g.FillEllipse(s, 63, 60, 5, 5);
+            }
+            else if (bh == bodyOfHangnam.mouth)// draw the  mouth with an arc   
+                g.DrawArc(p, 50, 60, 20, 20, 45, 90);
+
+            else if (bh == bodyOfHangnam.body)// draw the body
+                g.DrawLine(p, new Point(60, 90), new Point(60, 170));
+
+            else if (bh == bodyOfHangnam.leftArm)//draw the left Arm
+                g.DrawLine(p, new Point(60, 100), new Point(30, 85));
+
+            else if (bh == bodyOfHangnam.rightArm)// draw the right Arm
+                g.DrawLine(p, new Point(60, 100), new Point(90, 85));
+
+            else if (bh == bodyOfHangnam.leftLeg)// draw the left leg
+                g.DrawLine(p, new Point(60, 170), new Point(30, 190));
+
+            else if (bh == bodyOfHangnam.rightLeg)// Draw the right Leg
+                g.DrawLine(p, new Point(60, 170), new Point(90, 190));
+
+            else if (bh == bodyOfHangnam.nose)// Draw the nose
+            {
+                SolidBrush s = new SolidBrush(Color.Pink);
+                g.FillEllipse(s, 55, 69, 5, 5);
+
+            }
+
+        }
+
+
+        /* this method will generate the undercores 
+         * it will give random word to a variable
+         * will convert any string variable to a cha array(charArray)
+         * and will create the length of the word to put it as underscore
+         */
+        void DesignLabel()
+        {
+            Colors = RandomWord(); // this line is getting the random word 
+            char[] word = Colors.ToCharArray();//this line is casting the random word to a character
+            int between = 460 / word.Length;// this line is putting space between character and label
+
+
+            for (int loop = 0; loop < word.Length; loop++)// this for loop will iterate through each character 
+            {
+
+                List.Add(new Label());// this line is creating new labels
+                List[loop].Location = new Point((loop * between) + 20, 110);// specifying the location of the labels
+                List[loop].Text = "___"; // this line is specifiying that that the label should like "___"
+                List[loop].Parent = groupBox2;//set the parent of the control 
+                List[loop].BringToFront();// is bringing to front 
+                List[loop].CreateControl();// is creating a control 
+            }
+
+            //the text of lblNumber will display the length o the word to guess
+            lblNumber.Text = "" + (word.Length).ToString();
+
+        }
+
+
+
+
+        // populate my array to generate random word
+        string RandomWord()//this line will draw random colors
+        {
+            string[] colorList = new string[10];
+            colorList[0] = "BLUE";
+            colorList[1] = "YELLOW";
+            colorList[2] = "GRAY";
+            colorList[3] = "BROWN";
+            colorList[4] = "MAROON";
+            colorList[5] = "PINK";
+            colorList[6] = "BELGE";
+            colorList[7] = "PURPLE";
+            colorList[8] = "BLACK";
+            colorList[9] = "WHITE";
+            Random ranGen = new Random(); // instance of the random generator
+            var idx = ranGen.Next(0, 9);
+            return colorList[ranGen.Next(0, colorList.Length)];
+
+        }
+
+
+
+
+
+        private void HangmanGame_Shown(object sender, EventArgs e)
+        {
+            DrawHang();
+          
+            // calling the DrawHang method to draw the hang post
+
+        }
+
+
+
+
+
+        // thi mehod reset the game to a new state
+        void clearGame()
+        {
+            Graphics g = HangmanPost.CreateGraphics(); // recreate another graphics for the panel
+            g.Clear(HangmanPost.BackColor);//clear everything
+            DrawHang();// Redraw the hang post
+            RandomWord();// new random word
+            DesignLabel();// generate labels for the new word
+
+
+            lstBxscoreboard.Items.Clear();
+            score = 0;// this line is initializing or reseting the counter 
+            Number = 0;//reset the bodypart counter
+
+            lblSize.Text = "Wrong Letter : ";//clear and reset the missed guesses 
+            txtLetter.Text = "";
+        }
+
+
+
+        private void btnGuess_Click(object sender, EventArgs e)
+        {
+           
+          
+            score++;//increments the score variables by 1 
+            char l = txtLetter.Text.ToUpper().ToCharArray()[0];//Get the character that the user submitted, conversed to lowercase and to a variable
+            //only one element in properties
+            try
+            {
+
+                if ((Char.IsLetter(l)) && (Colors.Contains(l)))//Check if the character entered is actually a letter
+                {
+                    correctAnswer.Add(l);//Adds the letter to the correct ArrayList.
+                    //Create a character array of containing all the characters in the word.
+                    char[] ltts = Colors.ToCharArray();
+                    //Iterate through the entire array.
+                    for (int loop = 0; loop < ltts.Length; loop++)
+                    {
+                        if (ltts[loop] == l) //Check if the letter submitted is the correct letter.
+                            List[loop].Text = l.ToString();
+                    }
+
+
+                    //Check to see if the user has won 
+                    foreach (Label W in List)
+                        if (W.Text == "___") return;
+
+                    UpdateScore();//Updates the scoreboard and highscore list.
+
+
+                    MessageBox.Show("You Woon!!!!", "Congrats", MessageBoxButtons.OK, MessageBoxIcon.None);
+
+
+                }
+                else
+                {
+                    incorrectAnswer.Add(l);//Adds the letter to the incorrect ArrayList.
+
+                    MessageBox.Show("The word does not contain this letter!", "Try again!", MessageBoxButtons.RetryCancel);
+                    lblMissed.Text += " " + l.ToString() + " ,";
+                    //drawing the next bodypart upon incorrect guess.
+                    DrawBodyOfHangman((bodyOfHangnam)Number);
+                    Number++;
+                    lblLives.Text = "Lives used : " + Number.ToString() + "/10";
+
+
+                    //Reaching the final bodypart triggers the game lost message.
+                    if (Number == 10)
+                    {
+                        MessageBox.Show("you have lost,The Colors was " + this.Colors, "Oops!");
+                        clearGame(); //Call the method to reset the game state when you lose
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                if (char.IsNumber(l))//check if it is letter
+                {
+                    MessageBox.Show(ex.Message+"Please enter only Letter", "Error", MessageBoxButtons.RetryCancel, MessageBoxIcon.Warning);
+                    
+                }
+                
+            }
+            
+            
+        }
+
+        // This methods will updates the scoreboard and the highscore list.
+        private void UpdateScore()                                         //This method updates the scoreboard and the highscore list.
+        {
+            if (!scores.ContainsKey(Colors))                                     //Checks if the scores dictionary does not contain the current word's key.
+            {
+                scores.Add(Colors, score);                                       //Adds the score to the sortedList. *The dictionary version of it. 
+            }
+            else                                                               //Otherwise, set the value to score.
+            {
+                scores[Colors] = score;
+            }
+
+            if (!highScores.ContainsKey(Colors))                                 //Checks if the highscores dictionary does not contain the word's key.
+            {
+                highScores.Add(Colors, score);                                   //Adds the high score to the Dictionary for display later.
+            }
+
+            if (score < highScores[Colors])                                      //Check if the current score is lower than the previous high score.
+            {
+                highScores[Colors] = score;                                      //Replace the score in the highscore dictionary.
+            }
+
+            lstBxscoreboard.Items.Add(scores[Colors]);                                //Add the score entry to the scoreboardListBox.
+
+            UpdateHighScore();                                          //Update the high score listBox using the method.
+        }
+
+
+
+        private void UpdateHighScore()                                  //This method updates the high score listBox, displaying the high scores of each word attempted.
+        {
+            lstBxhighScore.Items.Clear();                                       //Clears the highscore listBox.
+
+            foreach (var word in highScores.Keys)                              //Iterates through the keys in highscores Dictionary.
+            {
+                var score = highScores[Colors];                                  //Sets the score variable to the highscore key's value.
+                lstBxhighScore.Items.Add($"{word}: {score}");                   //Adds the word and it's score as a string to display in the highScoresBox.
+            }
+        }
+
+        
+
+        private void btnRestart_Click(object sender, EventArgs e)
+        {
+            clearGame();//Reset the game 
+        }
+    }
+    /*this code will create a sub-form
+     *but it won't be use in this application
+    */
+
+    class startGame : Form
+    {
+        public startGame()
+        {
+            InitializeComponent();
+
+        }
+        void InitializeComponent()
+        {
+            try
+            {
+                this.Name = "HangmanGame";
+
+                this.Size = new System.Drawing.Size(700,433);
+                this.Text = "Hangman, Let Play";
+                this.BackgroundImage = System.Drawing.Bitmap.FromFile("C:/Users/jc12j/Downloads/hang.jpeg");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message," you cannot see the Background image");
+            }
+            
+        }
+        
+       
+        
+        
+       
+    }
+
+     
+}
